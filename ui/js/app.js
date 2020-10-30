d618e65d8d8415394cf2f8ed49393e83622c42c3
Yassine <yassine.elouri@gmail.com>
2020-10-29 23:34:08 -0700

    partition delete & edit + fixup

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,21 +1,24 @@
 import $ from 'jquery';
-import { EMPTY, from, fromEvent, merge, of } from 'rxjs';
-import { catchError, map, switchMap, tap } from 'rxjs/operators';
+import { EMPTY, from, fromEvent, merge, of, partition } from 'rxjs';
+import { catchError, map, pluck, switchMap, tap } from 'rxjs/operators';
 
 import { addIssue, deleteIssue, issues } from './release';
 
 
-const addIssueRow = issue => {
+const addRow = issue => {
     const { id, dev, estimate } = issue;
     const row = $('<tr>');
+    const cell = () => $('<td>').attr('data-type', 'model');
     const deleteIcon = $('<i>').addClass('fas fa-trash-alt');
-    row.append($('<td>').text(id));
-    row.append($('<td>').text(dev));
-    row.append($('<td>').text(estimate));
+    row.append(cell().text(id));
+    row.append(cell().text(dev));
+    row.append(cell().text(estimate));
     row.append($('<td>').append(deleteIcon));
     $('#release tbody').append(row);
     $('#newIssue').trigger('reset');
 };
+
+const deleteRow = row => $(`#release tbody tr:nth-of-type(${row})`).remove();
 
 merge(
     from(issues()),
@@ -36,9 +39,24 @@ merge(
             })
         ))
     )
-).subscribe(addIssueRow);
+).subscribe(addRow);
 
-fromEvent($('#release tbody'), 'click').pipe(
+const [edit$, delete$] = partition(
+    fromEvent($('#release tbody'), 'click'),
+    e => $(e.target.closest('td')).attr('data-type') === 'model');
+
+delete$.pipe(
+    map(e =>  {
+        return {
+            row: $(e.target.closest('td')).parent().index()+1,
+            id: $(e.target.closest('tr')).find(">:first-child").text()
+        };
+    }),
+    tap(r => deleteIssue(r.id)),
+    pluck('row')
+).subscribe(deleteRow);
+
+edit$.pipe(
     map(e => {
         return {
             row: $(e.target.closest('td')).parent().index()+1,
@@ -46,10 +64,4 @@ fromEvent($('#release tbody'), 'click').pipe(
             id: $(e.target.closest('tr')).find(">:first-child").text()
         };
     })
-).subscribe(r => {
-    if (r.column === 4) {
-        deleteIssue(r.id);
-        $(`#release tbody tr:nth-of-type(${r.row})`).remove();
-    }
-});
-
+).subscribe(console.log);
